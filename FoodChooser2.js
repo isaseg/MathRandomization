342a40972b8738a2bdc83a9fe5c906f446c93f80
isaseg <42422178+isaseg@users.noreply.github.com>
2018-08-20 12:16:52 -1000

    First Commit
    Messed up cooking method and food

---
+++ FoodChooser2.js
@@ -0,0 +1,8 @@
+//Array one is a cooking method
+var method = ["Steamed", "Fried", "Raw", "Stir fried", "Deep fried", "Smoked"];
+//Array two is a type of food
+var food = ["Spaghetti", "Seafood", "Steak", "Burger", "Curry", "Hot dog", "Chicken", "Rice", "Human", "Pork", "Bread", "POtato"];
+//Make the computer choose a food and way it is cooked
+var randOne = Math.floor(Math.random()*method.length);
+var randTwo = Math.floor(Math.random()*food.length);
+window.alert(method[randOne]+" "+food[randTwo]);
