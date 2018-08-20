faae2f2bb8f6919c967a6374f08a9f41f51bf91c
isaseg <42422178+isaseg@users.noreply.github.com>
2018-08-20 12:18:16 -1000

    Update FoodChooser2.js

--- FoodChooser2.js
+++ FoodChooser2.js
@@ -1,7 +1,7 @@
 //Array one is a cooking method
 var method = ["Steamed", "Fried", "Raw", "Stir fried", "Deep fried", "Smoked"];
 //Array two is a type of food
-var food = ["Spaghetti", "Seafood", "Steak", "Burger", "Curry", "Hot dog", "Chicken", "Rice", "Human", "Pork", "Bread", "POtato"];
+var food = ["Spaghetti", "Seafood", "Steak", "Burger", "Curry", "Hot dog", "Chicken", "Rice", "Human", "Pork", "Bread", "Potato"];
 //Make the computer choose a food and way it is cooked
 var randOne = Math.floor(Math.random()*method.length);
 var randTwo = Math.floor(Math.random()*food.length);
